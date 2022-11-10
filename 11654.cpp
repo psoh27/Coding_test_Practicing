b8141b0b163d1e7434993b9321f444cd7944db62
Sohyeon Park <61766465+psoh27@users.noreply.github.com>
2022-11-10 15:33:52 +0900

    아스키 코드

---
+++ 11654.cpp
@@ -0,0 +1,15 @@
+알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    char a;
+    cin>>a;
+    int num = a;
+    cout<<num;
+}
